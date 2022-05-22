d4253f50e31ae075c597f108d72dc307eb6d8e01
Lucas Carinhanha <lucascarinhanha4@gmail.com>
2022-05-22 11:13:24 -0400

    adicionado o InitialsModule nos modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PastasModule } from './pastas/pastas.module';
 import { UsersModule } from './users/users.module';
+import { InitialsModule } from './initials/initials.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest'),
     UsersModule,
     PastasModule,
+    InitialsModule,
   ],
   controllers: [],
   providers: [],
